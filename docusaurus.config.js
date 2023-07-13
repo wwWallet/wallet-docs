e3bb8b71646ef7959faf93d5e1afaa741e0406b8
Emmanouil Koukoularis <koukoularisman@gmail.com>
2023-07-13 14:46:33 +0300

    changed social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,7 +58,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/ediplomasLogo.png',
       navbar: {
         title: 'eDiplomas Wallet Ecosystem',
         logo: {
